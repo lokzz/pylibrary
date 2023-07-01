ec1f3b83e4827e633361f54cc7ffb6358d38764a
lokzz <50368061+lokzz@users.noreply.github.com>
2023-07-01 22:42:50 +0800

    ahh2

--- upload.bat
+++ upload.bat
@@ -1,1 +1,1 @@
-@envs.bat && python -m build && py -m twine upload -p %pytok% -u "__token__" dist\lokzz*
+@envs.bat && del /F /Q dist\* && python -m build && py -m twine upload -p %pytok% -u "__token__" dist\lokzz*
